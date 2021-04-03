d1bb0f7faee0c2d13ea02df12e86b73e030616b2
Mark <markhskinner@gmail.com>
2021-04-03 02:16:45 -0600

    added arduino sketch

---
+++ IRreciever/IRreciever.ino
@@ -0,0 +1,17 @@
+//pin 11
+#include <IRremote.h>
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+void loop() {
+  if (irrecv.decode(&results)) {
+    if(results.value != 0xFFFFFFFF)
+      Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+}
